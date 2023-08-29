f6634a7180f4b15bbab60a9884f9d2ace6ced40e
Jsparks1989 <jsparks19891@gmail.com>
2023-08-29 14:49:34 -0400

    Removed the code thay builds the query from getAllHunts and placed it in a separate class. This way the query building code can be reused.

--- controllers/huntController.js
+++ controllers/huntController.js
@@ -29,200 +29,234 @@
 const Hunt = require('../models/huntModel');
 
 
+/* ===========================================================================
+    HELPER FUNCTIONS/CLASSES
+=========================================================================== */
+/**
+ * Created a class for the API Features:
+ *   filtering, sorting, field limiting, pagination, etc.
+ * 
+ * This class builds the query and will execute the query in the controller functions.
+ *
+ * Two Parameters:
+ *   1. A query object (Tour.find() or User.find() since mongoose Model methods return query object).
+ *     Need to pass a query object as a param so we can chain other query object helper functions to it by running methods from APIFeature object.
+ *     By passing in a query object, we can chain other methods like sort() or find().
+ *   2. req.query - the query string thats coming from express.
+ * 
+ * Inside the class, this.query is the query object.
+ *   So in filter(), 'this.query = this.query.find(JSON.parse(queryStr));' is the same as 'Tour.find().find(JSON.parse(queryStr))'.
+ *   This is acceptable bc we havent executed the query yet.
+ *   We execute the query in the exported controller functions.
+ *
+ * Each of the methods will manipulate the query object and will add more methods to it.
+ *   Each method will set this.query to the query that method needs to execute.
+ *   Each method will also return 'this' so the methods can be chained when instantiating an APIFeature object.
+ *
+ * To execute the query, await APIFeature_object_variable_name.query.
+ *   Ex. const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+ *       const tours = await features.query;
+ */
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
 
+  // FILTER FUNCTIONALITY
+  /////////////////////////////////
+  /**
+   * FILTERING QUERIES
+   * -----------------------
+   * Use Model.find() method for filtering.
+   * 1. Make a hard copy of the query string
+   *
+   * 2. Make array of fields you dont want included in the query string.
+   *    Excluded fields will be handled in another method.
+   * 
+   * 3. Need to stringify the query so 'gt', 'gte', 'eq', 'lt', and 'lte' and be replaced with mongoDB operators using RegEx.
+   *
+   * Advanced filtering involves using operators, not just parameters.
+   *   Ex. 127.0.0.1:3000/api/v1/tours?limit[gte]=10
+   *
+   * MongoDB advanced filtering operators:
+   *   $gt (greaterThan) || $gte (greaterThanOrEqualTo) || $eq (equalTo) || $lt (lessThan) || $lte (lessThanOrEqualTo)
+   *   Ex. req.query -> { difficulty: 'easy', duration: { gte: '5' } }
+   *       mongoDB query -> { difficulty: 'easy', duration: {$gte: 5} }
+   *
+   * 4. After replacing the filter operators with mongoDB filter operators, need to convert query back to an object with JSON.parse().
+   *
+   * 5. Then set this.query = this.query.find(JSON.parse(queryStr)).
+   *    Need to chain find(JSON.parse(queryStr)) onto this.query. 
+   * 
+   * 6. return 'this' so we can chain the filter() method to the query when the query is being executed.
+   */
+  filter() {
+    const queryObj = { ...this.queryString };
+    console.log('hard copy queryObj:', queryObj);
+    const excludedFields = ['page', 'limit', 'sort', 'fields'];
 
+    excludedFields.forEach(field => delete queryObj[field]);
+
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+  }
+
+  // SORTING
+  ////////////////////////////////
+  /**
+   * SORTING
+   * -------
+   * Sort return results in ascending/descending order based on certain fields
+   *   Ex. 127.0.0.1:3000/api/v1/tours?sort=numOfPlayers (sort in ascending order)
+   *   Ex. 127.0.0.1:3000/api/v1/tours?sort=-numOfPlayers (sort in descending order)
+   *
+   * In instances when sorting and there are multiple items with the same value,
+   *   we can add a second field name to sort as a second criteria.
+   *   Ex. 127.0.0.1:3000/api/v1/tours?sort=ratingsAverage,ratingsQuantity
+   *
+   * 1. Check if sort is a property in the this.queryString object.
+   *
+   * 2. Create sortBy variable and set to this.queryString.sort split by comma and join them with a space.
+   *
+   * 3. Chain 'select(sortBy)' to the query.
+   *
+   * 4. Create a default sort in the else clause.
+   *   Will return all documents in a descending order by the createdAt field.
+   * 
+   * 5. Return 'this' outside if statement so we can chain the sort() method to the query when the query is being executed..
+   */
+  sort() {
+    if(this.query.sort) {
+      const sortBy = this.query.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  // FIELD LIMITING
+  ////////////////////////////////
+  /**
+   * FIELD LIMITING
+   * --------------
+   * The client chooses which fields they can get back in response.
+   *   Ex. 127.0.0.1:3000/api/v1/tours?fields=title,description,difficulty,completed
+   *     Only receiving the fileds specified by 'field' variable.
+   *   Ex. 127.0.0.1:3000/api/v1/tours?fields=-__v
+   *     The - means we want to exclude the field, not include it.
+   *
+   * Its best practices to limit the amount of data returned so we limit the use of bandwidth.
+   *
+   * We can also limit what fields are returned from the query in the Schema with the 'select' property.
+   *
+   * 1. Check if 'field' is in this.queryString.
+   *
+   * 2. Then create new variable (fields) set to separate the fields by comma and join with a space.
+   *   Ex. this.queryString.fields.split(',').join(' ');
+   *
+   * 3. Then chain 'select(fields)' to the query.
+   *
+   * 4. Create a default field limit in the else clause.
+   *   Want to return all the fields EXCEPT the '__v' field.
+   * 
+   * 5. return 'this' so we can chain the limitFields() method to the query when the query is being executed.
+   */
+  limitFields() {
+    if(this.queryString.fields) {
+      let fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  // PAGINATION
+  /////////////////////////////////
+  /**
+   * PAGINATION
+   * ----------
+   * Figures out the number of pages to return results and how many results on each page.
+   *   Ex. 127.0.0.1:3000/api/v1/tours?page=2&limit=10
+   *
+   * ?page=2&limit=10
+   * query = query.skip(10).limit(10);
+   * Use the skip() and limit() methods when the user chooses a certain page.
+   *   If the limit of results for each page is 10, and the user wants to be on page 2,
+   *   then we need to skip the first 10 results so we start with results 11-20.
+   *   If the user wants page 5, then we need to skip the first 40 results and return 41-50.
+   *
+   * If the user requests a page that doesnt exist bc theres not enough results to have the requested page,
+   *   First make sure this.queryString.page exists,
+   *   Then await for Model.countDocuments() method to return total amount of documents for the model.
+   *   If the number of documents skipped is greater than or equal to total documents, then the page doesnt exist and throw an error.
+   *
+   * 1. Define 'page' variable and set to value by multiplying this.queryString.page by 1 to convert the string to number.
+   *   Use || to define default value if there is no page field in this.queryString.
+   *
+   * 2. Define 'limit' variable and set to value by multiplying this.queryString.limit by 1 to convert the string to number.
+   *    Use || to define default value if there is no limit field in this.queryString.
+   *
+   * 3. Define 'skip' variable and set to value by muliplying the previous page number by the limit.
+   *    const skip = (page - 1) * limit;
+   *
+   * 4. Chain skip(skip_var) and limit(limit_var) methods to the query.
+   *    this.query = this.query.skip(skip).limit(limit);
+   *
+   * 5. return 'this' so we can chain the paginate() method to the query when the query is being executed. 
+   */
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page -1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+  }
+}
 
 /* ===========================================================================
     CONTROLLER FUNCTIONS
 =========================================================================== */
 /**
- * Getting all the Hunt documents
- * ------------------------------
- * BUILD THE QUERY
- * ---------------
- * 1. Make a hard copy of req.query (dont want to affect the original query)
- *   so we can remove exculuded fields and handle them separately.
- *
- * 2. Remove the excluded fields from the query copy.
- *   foreach will return the same queryObj with all excluded fields removed.
- * -------------------------------------------------------------------------------------------
- * BASIC FILTERING QUERIES
- * -----------------------
- * 1. After query copy has excluded fields removed, we can run basic filtering with Model.find().
- *   Run it after advanced filtering has been handled.
- * Ex. 127.0.0.1:3000/api/v1/scavhunt?difficulty=easy
- * -------------------------------------------------------------------------------------------
- * ADVANCED FILTERING QUERIES
- * --------------------------
- * Advanced filtering involves using operators, not just parameters.
- * Ex. 127.0.0.1:3000/api/v1/scavhunt?numOfPlayers[gte]=10
+ * Gets all the Hunt documents based on the fields in the req.query.
  * 
- * MongoDB advanced filtering operators
- *   $gt (greaterThan) || $gte (greaterThanOrEqualTo) || $eq (equalTo) || $lt (lessThan) || $lte (lessThanOrEqualTo)
+ * Uses the APIFeatures class to build the query for Hunt documents.
+ *   The APIFeatures class takes a query object and req.query as params,
+ *   each of the methods chains a query object function to the query object passed in as a param.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 1. Build the query.
+ *   Instantiate a new APIFeature object and pass a query object (Tour.find()) and the query string (req.query) as params.
+ *   Will chain the APIFeature methods to the new instantiated object.
+ *   Each of the methods will chain a helper function to this.query (the query object passed in as a param; Tour.find()).
  * 
- * req.query -> { difficulty: 'easy', duration: { gte: '5' } }
- * mongoDB query -> { difficulty: 'easy', duration: {$gte: 5} }
+ * 2. Execute the query. 
+ *   Create 'tours' variable and set it to await the execution of the APIFeature object query.
+ *   Execute the query using await.
  * 
- * 1. Need to stringify the query so 'gt', 'gte', 'eq', 'lt', and 'lte' and be replaced with mongoDB operators using RegEx.
- *   String.replace() returns a new string.
- * 
- * 2. After replacing the filter operators with mongoDB filter operators, need to convert query back to an object.
- * 
- * 3. Then chain the Query.find() method to the query.
- * -------------------------------------------------------------------------------------------
- * SORTING
- * -------
- * Sort return results in ascending/descending order based on certain fields
- *   Ex. 127.0.0.1:3000/api/v1/scavhunt?sort=numOfPlayers (sort in ascending order)
- *   Ex. 127.0.0.1:3000/api/v1/scavhunt?sort=-numOfPlayers (sort in descending order)
- * 
- * In instances when sorting and there are multiple items with the same value,
- *   we can add a second field name to sort as a second criteria.
- *   Ex. 127.0.0.1:3000/api/v1/scavhunt?sort=numOfPlayers,numOfItems
- * 
- * 1. Check if sort is a property in the req.query object.
- *
- * 2. Create new variable (sortBy) thats set to req.query.sort split by comma and join them with a space.
- *
- * 3. Chain select(sortBy) to the query.
- *
- * 4. Create a default sort in the else clause.
- *   Will return all documents in a descending order by the createdAt field.
- * -------------------------------------------------------------------------------------------
- * FIELD LIMITING
- * --------------
- * The client chooses which fields they can get back in response.
- *   Ex. 127.0.0.1:3000/api/v1/scavhunt?fields=title,description,difficulty,completed
- *     Only receiving the fileds specified by 'field' variable.
- *   Ex. 127.0.0.1:3000/api/v1/scavhunt?fields=-__v
- *     The - means we want to exclude the field, not include it.
- *
- * Its best practices to limit the amount of data returned so we limit the use of bandwidth.
- *
- * We can also limit what fields are returned from the query in the Schema with the 'select' property.
- *
- * 1. Check if 'field' is in req.query.
- *     
- * 2. Then create new variable (fields) set to separate the fields by comma and join with a space.
- *   Ex. req.query.fields.split(',').join(' ');
- * 
- * 3. Then chain select(fields) to the query.
- *
- * 4. Create a default field limit in the else clause.
- *   Want to return all the fields EXCEPT the '__v' field.
- * -------------------------------------------------------------------------------------------
- * PAGINATION
- * ----------
- * Figures out the number of pages to return results and how many results on each page.
- *   Ex. 127.0.0.1:3000/api/v1/scavhunt?page=2&limit=10
- * 
- * ?page=2&limit=10
- * query = query.skip(10).limit(10);
- * Use the skip() and limit() methods when the user chooses a certain page.
- *   If the limit of results for each page is 10, and the user wants to be on page 2,
- *   then we need to skip the first 10 results so we start with results 11-20.
- *   If the user wants page 5, then we need to skip the first 40 results and return 41-50.
- * 
- * If the user requests a page that doesnt exist bc theres not enough results to have the requested page,
- *   First make sure req.query.page exists,
- *   Then await for Model.countDocuments() method to return total amount of documents for the model.
- *   If the number of documents skipped is greater than or equal to total documents, then the page doesnt exist and throw an error.
- *
- * 1. Define variable for page value by multiplying req.query.page by 1 to convert the string to number.
- *   Use || to define default value.
- * 
- * 2. Define variable for limit values by multiplying req.query.limit by 1 to convert the string to number.
- *    Use || to define default value.
- * 
- * 3. Define the skip value by muliplying the previous page number by the limit.
- * 
- * 4. Chain skip(skip_var) and limit(limit_var) methods to the query.
- * 
- * 5. Check if req.query.page is true. (To make sure the user hasnt requested a page that doesnt exist)
- * 
- * 6. Then await Hunt.countDocuments() to return the total number of all Hunt documents and set to variable.
- * 
- * 7. Compare the number of skipped to the total number of Hunt documents
- * 
- * 8. If the number of skipped >= total Hunt documents, throw new error 'This page does not exist.' 
- *
- * -------------------------------------------------------------------------------------------
- * EXECUTE THE QUERY
- * -----------------
- * Only want to await when executing the query after all the chaining from basic/advanced filtering, sorting, field limiting, and pagination are added.
+ * 3. Send response.
  */
 exports.getAllHunts = async (req, res) => {
   try {
     console.log('req.query:', req.query);
 
-    // BUILD THE QUERY & BASIC FILTERING
+    // BUILD THE QUERY
     /////////////////////////////////
-    //=== Hard copy of req.query
-    const queryObj = { ...req.query };
-
-    //=== Excluded fields
-    const excludedFields = ['page', 'limit', 'sort', 'fields'];
-
-    //=== Remove excluded fields from the request query
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-
-    // ADVANCED FILTERING
-    /////////////////////////////////
-    /*
-      No Need for advanced filtering yet.
-      No fileds to be used by advanced filtering yet
-
-      1. Stringify queryObj and set to variable.
-      2. Use RegEx to replace the filter operators in URL (gte, gt, lte, lt)
-         with mongoDB filtering operators ($gte, $gt, $lte, $lt).
-      3. Parse queryObj back to object.
-      4. Create a query variable and set it equal to the Hunt model and chain find() to it with queryObj passed in as param.
-        Ex. let query = Hunt.find(queryObj);
-
-    */ 
-
-    // SORTING
-    /////////////////////////////////
-    /*
-      No Need for sorting yet.
-      No fileds to be used for sorting yet
-
-      1. Check if req.query.sort is true.
-      2. In case there are multiple fields for sort, split() req.query.sort by comma and then join() with a space; set to variable.
-        Ex. const sortBy = req.query.sort.split(',').join(' ');
-      3. Chain Query.sort() to the query and pass sortBy in as param.
-      4. Add default sorting in else clause
-        Sort by createdAt in descending order.
-
-     */ 
-
-    // FIELD LIMITING
-    /////////////////////////////////
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // PAGINATION
-    /////////////////////////////////
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page -1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numHunts = await hunts.countDocuments();
-      if (skip >= numHunts) throw new Error('This page does not exist');
-    }
-
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
     // EXECUTE THE QUERY
     /////////////////////////////////
-    const hunts = await query;
+    const tours = await features.query;
 
     // SEND RESPONSE
     /////////////////////////////////
